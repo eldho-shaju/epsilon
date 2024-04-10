070a1a6723c55b78a341e0b62623ab94b7473deb
eldho-shaju <eldhoshajumukkath@gmail.com>
2024-04-10 16:30:26 +0530

    removed unwanted firebase variables

--- src/Page/Admin/adminPanel.js
+++ src/Page/Admin/adminPanel.js
@@ -1,21 +1,21 @@
-import { Box, Typography } from "@mui/material";
-import React from "react";
-import useDeviceTypeCheck from "../../Hooks/useDeviceTypeCheck";
-import AdminForm from "./adminForm";
+// import { Box, Typography } from "@mui/material";
+// import React from "react";
+// import useDeviceTypeCheck from "../../Hooks/useDeviceTypeCheck";
+// import AdminForm from "./adminForm";
 
-const AdminPanel = () => {
-  const { isDesktop } = useDeviceTypeCheck();
+// const AdminPanel = () => {
+//   const { isDesktop } = useDeviceTypeCheck();
 
-  return (
-    <section>
-      <div>
-        <h4>Admin</h4>
-        <div>
-          <AdminForm />
-        </div>
-      </div>
-    </section>
-  );
-};
+//   return (
+//     <section>
+//       <div>
+//         <h4>Admin</h4>
+//         <div>
+//           <AdminForm />
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
 
-export default AdminPanel;
+// export default AdminPanel;
